b1e2cbe3da8b80918bd6e1bdac10996ed01938c2
Unknown <agustina.buccella@fi.uncoma.edu.ar>
2018-10-05 19:28:19 -0300

    p-datatable for products

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { AppComponent } from './app.component';
 import { AuthGuard } from './shared';
 
 const routes: Routes = [
-    { path: '', loadChildren: './layout/layout.module#LayoutModule', canActivate: [AuthGuard] },
+    { path: '', loadChildren: './layout/layout.module#LayoutModule', canActivate: [AuthGuard] }, // aca llama al layout del costado
     { path: 'login', loadChildren: './login/login.module#LoginModule' },
     { path: 'error', loadChildren: './server-error/server-error.module#ServerErrorModule' },
     { path: 'access-denied', loadChildren: './access-denied/access-denied.module#AccessDeniedModule' },
